5265eeaefd335610e82e11b507d4f0feb923d25d
David Adam Justice <david@outthinkgroup.com>
2014-04-18 15:30:56 -0700

    data in da browser

--- js/app.js
+++ js/app.js
@@ -4,8 +4,42 @@ App.Router.map(function() {
   // put your routes here
 });
 
+App.medicalCaseData = Ember.Object.extend({});
+var link = App.medicalCaseData.create();
+link.set('url', 'http://localhost:8000/data.json');
+
+var stubData;
+console.log(link.get('url'));
+$.getJSON(link.get('url'), function(response) {
+  stubData = reorganizeData(response);
+  console.log(stubData);
+});
+
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return stubData;
   }
 });
+
+function reorganizeData(data) {
+  var procedures = data.Procedure || [];
+
+  var accessSites = procedures.filter(function (proc) {
+    return (proc.ProcedureTypeId === 1);
+  });
+
+  procedures.forEach(function (proc) {
+    if (proc.ParentProcedureId) {
+      accessSites.forEach(function (accessSite) {
+        accessSite.procedures = accessSite.procedures || [];
+        if (proc.ParentProcedureId === accessSite.ProcedureId) {
+          accessSite.procedures.push(proc);
+        }
+      });
+    }
+  });
+
+  data.accessSites = accessSites;
+
+  return data;
+}
